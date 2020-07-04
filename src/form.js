90d9cd0ca8cccab2c1fa84a24606fc6bcb75e00f
Brittany Javalera <brittjavalera@gmail.com>
2020-07-04 12:36:59 -0700

    added id's to input field for easier access in js validations. Created js validation function with variable for each input field

---
+++ src/form.js
@@ -0,0 +1,19 @@
+function validate(){
+    const firstName = document.getElementById("first-name")
+    const lastName = document.getElementById("last-name")
+    const email = document.getElementById("email")
+    const password = document.getElementById("password")
+
+    if (firstName.value == ""){
+
+    }
+    if (lastName.value == ""){
+        
+    }
+    if (email.value == ""){
+        
+    }
+    if (password.value == ""){
+        
+    }
+}
